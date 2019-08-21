38e8ea95e54221a954b87efd947c5a83ef86e3e0
Smith <smithwebdd@gmail.com>
2019-08-21 10:14:57 -0400

    created helper for nearby method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nearby(lng1, lat1, lng2, lat2)
+    if (lng1-lng2).abs <= 0.01 && (lat1-lat2).abs <= 0.01
+      return true
+    else
+      return false
+    end
+  end
 end
