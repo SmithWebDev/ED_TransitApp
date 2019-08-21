b107abdfe687098f16a888a0cdb8555e0f4feb6e
Smith <smithwebdd@gmail.com>
2019-08-21 09:31:55 -0400

    created Location Scaffold

---
+++ db/migrate/20190821133054_create_locations.rb
@@ -0,0 +1,12 @@
+class CreateLocations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :locations do |t|
+      t.string :address
+      t.string :city
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
